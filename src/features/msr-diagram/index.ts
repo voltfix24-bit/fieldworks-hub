256b13f8045cafdd333989495d2c986efe57c542
voltfix24-bit <voltfix24@gmail.com>
2026-06-20 19:39:47 +0200

    Export MSR anchor diagram type

--- src/features/msr-diagram/index.ts
+++ src/features/msr-diagram/index.ts
@@ -4,5 +4,5 @@ export { DiagramToolbar } from './Toolbar';
 export { DoorSideSelector } from './DoorSideSelector';
 export { ZoomControls } from './ZoomControls';
 export { renderDiagramToPng } from './export-png';
-export type { MSRDiagram, DiagramElectrode, DiagramCabinet, DoorSide } from './types';
+export type { MSRDiagram, DiagramElectrode, DiagramCabinet, DoorSide, MSRAnchor } from './types';
 export { DEFAULT_DIAGRAM } from './types';
